97fc8b3b13675fdabbcbfc20bab706ba45326126
Lewis Rye <lewisryework@gmail.com>
2024-06-21 20:01:35 +0100

    starting on the api

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,46 +1,133 @@
-use diesel::{Connection, ExpressionMethods, QueryDsl, RunQueryDsl, SqliteConnection};
-use dotenv::dotenv;
-use schema::customer_orders::order_date;
+mod models;
+
+use std::net::SocketAddr;
 use std::env;
-use chrono::{Duration, NaiveDate, NaiveDateTime, prelude::*};
-use serde::Serialize;
-use serde_json;
 
-pub mod schema;
-pub mod models;
+use chrono::{Utc};
+use http_body_util::{combinators::BoxBody, BodyExt, Full, Empty};
+use hyper::body::{Bytes};
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{Request, Response, Method, StatusCode};
+use hyper_util::rt::TokioIo;
+use models::Product;
+use tokio::net::TcpListener;
+use dotenv::dotenv;
 
-use self::models::*;
-use self::schema::access_level::dsl::access_level;
-use self::schema::account::dsl::account;
-use self::schema::category::dsl::category;
-use self::schema::customer::dsl::customer;
-use self::schema::customer_order_details::dsl::customer_order_details;
-use self::schema::customer_orders::dsl::customer_orders;
-use self::schema::product::dsl::product;
-
-fn get_orders_between_dates(
-    conn: &mut SqliteConnection,
-    start_date: NaiveDateTime,
-    end_date: NaiveDateTime,
-) -> Result<Vec<CustomerOrder>, diesel::result::Error> {
-    customer_orders
-        .filter(order_date.between(start_date, end_date))
-        .load::<CustomerOrder>(conn)
-}
-
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     dotenv().ok();
 
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let mut connection = SqliteConnection::establish(&database_url)
-        .expect(&format!("Error connecting to {}", database_url));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
-    let results: Result<Vec<CustomerOrder>, diesel::result::Error> = get_orders_between_dates(&mut connection, Utc::now().naive_utc() - Duration::days(7), Utc::now().naive_utc());
+    // We create a TcpListener and bind it to 127.0.0.1:3000
+    let listener = TcpListener::bind(addr).await?;
 
-    if let Ok(data) = results {
-        let json = serde_json::to_string(&data).unwrap();
-        println!("Serialized JSON: {}", json);
-    } else {
-        println!("Error occurred");
+    println!("Server online at: {}", Utc::now());
+
+    // We start a loop to continuously accept incoming connections
+    loop {
+        let (stream, _) = listener.accept().await?;
+
+        // Use an adapter to access something implementing `tokio::io` traits as if they implement
+        // `hyper::rt` IO traits.
+        let io = TokioIo::new(stream);
+
+        // Spawn a tokio task to serve multiple connections concurrently
+        tokio::task::spawn(async move {
+            // Finally, we bind the incoming connection to our `hello` service
+            if let Err(err) = http1::Builder::new()
+                // `service_fn` converts our function in a `Service`
+                .serve_connection(io, service_fn(handle_request))
+                .await
+            {
+                eprintln!("Error serving connection: {:?}", err);
+            }
+        });
     }
+}
+
+async fn handle_request(
+    req: Request<hyper::body::Incoming>,
+) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
+    match (req.method(), req.uri().path().split("/").nth(1)) {
+        (&Method::POST, Some("echo")) => Ok(Response::new(req.into_body().boxed())),
+
+        (&Method::GET, Some("product")) => {
+            let parts: Vec<&str> = req.uri().path().split("/product/").collect();
+            let mut id: i64 = 0;
+
+            // Extract the path parameters
+            if let Some(product_id_str) = parts.get(1) {
+                if let Ok(product_id) = product_id_str.parse::<i64>() {
+                    id = product_id;
+                }
+            }
+
+            println!("Requested Product ID: {}", id);
+
+            // Create a JSON value
+            let product = get_product(&id);
+
+            // Convert JSON to bytes
+            let json = serde_json::to_string(&product).unwrap();
+
+            // Build the HTTP response
+            Ok(Response::new(full(json)))
+        },
+
+        // Return 404 Not Found for other routes.
+        _ => {
+            let mut not_found = Response::new(empty());
+            *not_found.status_mut() = StatusCode::NOT_FOUND;
+            Ok(not_found)
+        }
+    }
+}
+
+fn get_product(id: &i64) -> Option<Product> {
+    if *id > 0 {
+        let query = "SELECT * FROM product WHERE product.product_id = ?;";
+
+        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+        let connection = sqlite::open(database_url).unwrap();
+        for row in connection
+            .prepare(query)
+            .unwrap()
+            .into_iter()
+            .bind((1, *id))
+            .unwrap()
+            .map(|row| row.unwrap())
+        {
+            let id: i64 = row.read("product_id");
+            let name: String = row.read::<&str, _>("product_name").to_string();
+            let cid: i64 = row.read("category_id");
+            let nis: i64 = row.read("number_in_stock");
+            let bp: i64 = row.read("buy_price");
+            let disc: i64 = row.read("discontinued");
+            println!("Found: {}, {}, {}, {}, {}, {}", id, name, cid, nis, bp, disc); // for debugging
+            let product = models::Product {
+                product_id: id,
+                product_name: name,
+                category_id: cid,
+                number_in_stock: nis,
+                buy_price: bp,
+                discontinued: disc,
+            };
+
+            return Some(product);
+        }
+    }
+    return None
+}
+
+fn empty() -> BoxBody<Bytes, hyper::Error> {
+    Empty::<Bytes>::new()
+        .map_err(|never| match never {})
+        .boxed()
+}
+fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
+    Full::new(chunk.into())
+        .map_err(|never| match never {})
+        .boxed()
 }
--- api/src/models.rs
+++ api/src/models.rs
@@ -1,59 +1,58 @@
 use chrono::NaiveDateTime;
-use diesel::Queryable;
 use serde::{Serialize, Deserialize};
 
-#[derive(Deserialize, Serialize, Queryable)]
+#[derive(Serialize, Deserialize)]
 pub struct AccessLevel {
-    pub level_id: Option<i32>,
-    pub name: Option<String>,
+    pub level_id: i64,
+    pub name: String,
 }
 
-#[derive(Deserialize, Serialize, Queryable)]
+#[derive(Serialize, Deserialize)]
 pub struct Account {
-    pub account_id: Option<i32>,
-    pub fname: Option<String>,
-    pub lname: Option<String>,
-    pub address: Option<String>,
-    pub level_id: Option<i32>,
-    pub username: Option<String>,
-    pub passhash: Option<String>,
+    pub account_id: i64,
+    pub fname: String,
+    pub lname: String,
+    pub address: String,
+    pub level_id: i64,
+    pub username: String,
+    pub passhash: String,
 }
 
-#[derive(Deserialize, Serialize, Queryable)]
+#[derive(Serialize, Deserialize)]
 pub struct Category {
-    pub category_id: Option<i32>,
-    pub category_name: Option<String>,
+    pub category_id: i64,
+    pub category_name: String,
 }
 
-#[derive(Deserialize, Serialize, Queryable)]
+#[derive(Serialize, Deserialize)]
 pub struct Customer {
-    pub customer_id: Option<i32>,
-    pub fname: Option<String>,
-    pub lname: Option<String>,
-    pub address: Option<String>,
-    pub phone: Option<String>,
+    pub customer_id: i64,
+    pub fname: String,
+    pub lname: String,
+    pub address: String,
+    pub phone: String,
 }
 
-#[derive(Deserialize, Serialize, Queryable)]
+#[derive(Serialize, Deserialize)]
 pub struct CustomerOrderDetails {
-    pub order_id: Option<i32>,
-    pub product_id: Option<i32>,
-    pub quantity: Option<i32>,
+    pub order_id: i64,
+    pub product_id: i64,
+    pub quantity: i64,
 }
 
-#[derive(Deserialize, Serialize, Queryable)]
+#[derive(Serialize, Deserialize)]
 pub struct CustomerOrder {
-    pub order_id: Option<i32>,
-    pub customer_id: Option<i32>,
-    pub order_date: Option<NaiveDateTime>,
+    pub order_id: i64,
+    pub customer_id: i64,
+    pub order_date: NaiveDateTime,
 }
 
-#[derive(Deserialize, Serialize, Queryable)]
+#[derive(Serialize, Deserialize)]
 pub struct Product {
-    pub product_id: Option<i32>,
-    pub product_name: Option<String>,
-    pub category_id: Option<i32>,
-    pub number_in_stock: Option<i32>,
-    pub buy_price: Option<f64>,
-    pub discontinued: Option<bool>,
+    pub product_id: i64,
+    pub product_name: String,
+    pub category_id: i64,
+    pub number_in_stock: i64,
+    pub buy_price: i64,
+    pub discontinued: i64,
 }
